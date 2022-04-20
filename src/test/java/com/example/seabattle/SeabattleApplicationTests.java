9849c1246d059bdc721358fa2dc3ac96520198f1
ermakovss.22 <sergeyermakov13112004@gmail.com>
2022-04-20 12:48:23 +0300

    Аы

---
+++ src/test/java/com/example/seabattle/SeabattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seabattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeabattleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
